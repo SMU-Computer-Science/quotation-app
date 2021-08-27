0543c31fdeba7474a52761cde3f8c16f2b6c946b
keng-swee <dreamsblaz@gmail.com>
2021-08-27 11:24:11 +0800

    added ocean rates

--- App.java
+++ App.java
@@ -6,18 +6,38 @@ public class App{
         Scanner sc = new Scanner(System.in);
 
         while(true) {
-            System.out.println("Welcome to Quotation App. Please select your product:");
+            System.out.println("Welcome to Quotation App. Please enter the number corresponding to your product choice.");
             System.out.println("1. Air");
             System.out.println("2. Ocean");
             System.out.println("3. Land");
+            System.out.print("Selection: ");
             
             int product = sc.nextInt();
-            if (product != 1) {
-                System.out.println("Sorry, the selected product is not available. Please choose a different option.");
+            if (product == 3) {
+                System.out.println("Sorry, the selected product is currently not available. Please choose a different option.");
+                product = sc.nextInt();
+                sc.nextLine();
+            } else if (product == 2) {
+                rateOutputOcean();
+            } else if (product == 1) {
+                rateOutputAir();
+            } else {
+                System.out.println("Sorry, you have entered an invalid option. Please try again.");
                 product = sc.nextInt();
                 sc.nextLine();
             }
-            
+
+            if(toExit()) {
+                break;
+            }
+        }
+
+        System.out.println("Thank you very much and have a nice day! :)");
+        sc.close();
+    }
+    
+    private static void rateOutputAir(){
+            Scanner sc = new Scanner(System.in);
             System.out.println("You have selected 'Air'.");
             System.out.print("Enter Weight: ");
             double weight = sc.nextDouble();
@@ -46,24 +66,54 @@ public class App{
                 rate = 10000;
                 break;
             }
-            
-            double price = weight * rate;
-            System.out.println();
-            System.out.printf("Price: $%.2f\n", price);
-            
-            System.out.println("Do you wish to calculate again?(Y/N)");
-            String s = sc.nextLine();
-            while(!(s.equals("Y") || s.equals("N"))) {
-                System.out.println("Invalid input.");
-                System.out.println("Do you wish to calculate again?(Y/N)");
-                s = sc.nextLine();
-            } 
-            if(s.equals("N")) {
-                break;
-            }
+            System.out.println(weight * rate);
         }
 
-        System.out.println("Thank you very much and have a nice day! :)");
-        sc.close();
+    private static void rateOutputOcean(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("You have selected 'Ocean'.");
+            System.out.print("Enter Volume: ");
+            double volume = sc.nextDouble();
+            sc.nextLine();
+            System.out.println("Select Unlocode (Please choose a number from 1 to 3):");
+            System.out.println("1. CNSHA - DEHAM (AP to North Europe)");
+            System.out.println("2. SGSIN - USLAX (AP to North America West Coast)");
+            System.out.println("3. Others");
+            int no = sc.nextInt();
+            sc.nextLine();
+            while(no < 0 || no > 3){
+                System.out.println("Select Region (Please choose a number from 1 to 3):");
+                no = sc.nextInt();
+            }
+            int rate = 0;
+            int rateArr[] = {920, 1093, 1000};
+            rate = rateArr[rate];
+            switch(no){
+                case 0:
+                rate = 920;
+                break;
+                case 1:
+                rate = 1093;
+                break;
+                case 2:
+                rate = 1000;
+                break;
+            }
+            System.out.println(volume * rate);
+    }
+
+    private static boolean toExit() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you wish to calculate again?(Y/N)");
+        String input = sc.nextLine();
+        while(!(input.equals("Y") || input.equals("N") || input.equals("y") || input.equals("n"))) {
+            System.out.println("Invalid input.");
+            System.out.println("Do you wish to calculate again?(Y/N)");
+            input = sc.nextLine();
+        } 
+        if(input.equals("N") || input.equals("n")) {
+            return true;
+        }
+        return false;
     }
 }
